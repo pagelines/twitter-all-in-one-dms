37daf1d3977a8d532ee2bc426017ef2ab2bced16
Chris Johnston <MrFent@gmail.com>
2013-11-27 11:36:20 -0600

    Added custom language support

--- sections/twitter-all-in-one-dms/section.php
+++ sections/twitter-all-in-one-dms/section.php
@@ -5,7 +5,7 @@ Author: MrFent
 Author URI: http://www.MrFent.com/twitter-all-in-one
 Demo: http://www.MrFent.com/twitter-all-in-one
 External: http://www.MrFent.com/twitter-all-in-one
-Version: 1.0.0
+Version: 1.0.1
 Description: Combines all four Twitter widgets: User timeline, Favorites, List, & Search
 Class Name: TwitterAllInOneDMS
 Workswith: templates, main, sidebar1, sidebar2, sidebar_wrap, header, footer, morefoot
@@ -107,11 +107,50 @@ class TwitterAllInOneDMS extends PageLinesSection {
 					'label' => __( 'Number of Tweets', 'twitter-all-in-one-dms' )
 				));	
 				}
+				
+		$Preferences_D = array(
+				array(
+					'key'	=> 'twlanguage',
+					'type' 	=> 'select',
+					'label'	=> __( 'Language', 'twitter-all-in-one-dms' ),
+					'help'	=> __( 'The widget language is detected from the page, based on the HTML lang attribute of your content. However you can set a custom language here.', 'twitter-all-in-one-dms' ),
+					'opts'	=> array(
+								'ar' 	=> array('name' => __( 'Arabic', 'twitter-all-in-one-dms' )),
+								'zh-cn' => array('name' => __( 'Simplified Chinese (China)', 'twitter-all-in-one-dms' )),
+								'zh-tw' => array('name' => __( 'Traditional Chinese (Taiwan)', 'twitter-all-in-one-dms' )),
+								'da' 	=> array('name' => __( 'Danish', 'twitter-all-in-one-dms' )),
+								'nl' 	=> array('name' => __( 'Dutch', 'twitter-all-in-one-dms' )),
+								'en' 	=> array('name' => __( 'English', 'twitter-all-in-one-dms' )),
+								'fil' 	=> array('name' => __( 'Filipino', 'twitter-all-in-one-dms' )),
+								'fi' 	=> array('name' => __( 'Finnish', 'twitter-all-in-one-dms' )),
+								'fr' 	=> array('name' => __( 'French', 'twitter-all-in-one-dms' )),
+								'de' 	=> array('name' => __( 'German', 'twitter-all-in-one-dms' )),
+								'he' 	=> array('name' => __( 'Hebrew', 'twitter-all-in-one-dms' )),
+								'hi' 	=> array('name' => __( 'Hindi', 'twitter-all-in-one-dms' )),
+								'hu' 	=> array('name' => __( 'Hungarian', 'twitter-all-in-one-dms' )),
+								'id' 	=> array('name' => __( 'Indonesian', 'twitter-all-in-one-dms' )),
+								'it' 	=> array('name' => __( 'Italian', 'twitter-all-in-one-dms' )),
+								'ja' 	=> array('name' => __( 'Japanese', 'twitter-all-in-one-dms' )),
+								'ko' 	=> array('name' => __( 'Korean', 'twitter-all-in-one-dms' )),
+								'msa' 	=> array('name' => __( 'Malay', 'twitter-all-in-one-dms' )),
+								'no' 	=> array('name' => __( 'Norwegian', 'twitter-all-in-one-dms' )),
+								'fa' 	=> array('name' => __( 'Persian (Farsi)', 'twitter-all-in-one-dms' )),
+								'pl' 	=> array('name' => __( 'Polish', 'twitter-all-in-one-dms' )),
+								'pt' 	=> array('name' => __( 'Portuguese', 'twitter-all-in-one-dms' )),
+								'ru' 	=> array('name' => __( 'Russian', 'twitter-all-in-one-dms' )),
+								'es' 	=> array('name' => __( 'Spanish', 'twitter-all-in-one-dms' )),
+								'sv' 	=> array('name' => __( 'Swedish', 'twitter-all-in-one-dms' )),
+								'th' 	=> array('name' => __( 'Thai', 'twitter-all-in-one-dms' )),
+								'tr' 	=> array('name' => __( 'Turkish', 'twitter-all-in-one-dms' )),
+								'ur' 	=> array('name' => __( 'Urdu', 'twitter-all-in-one-dms' )),
+					)));	
 		$Preferences_C = array_merge( $Preferences_A, $Preferences_B );
+		$Preferences_E = array_merge( $Preferences_C, $Preferences_D );
+		
 		$options[] = array(
 				'title' => __( 'Preferences', 'twitter-all-in-one-dms' ),
 				'type'	=> 'multi',
-				'opts'	=> $Preferences_C
+				'opts'	=> $Preferences_E
 			);
 		$Appearance_A = array(
 				array(
@@ -195,6 +234,8 @@ class TwitterAllInOneDMS extends PageLinesSection {
 	$twitterheight = str_replace(array('px', '%', ' '), array(''), ($this->opt('twheight')) ? $this->opt('twheight') : '600');
 	$twitterlinks = ( $this->opt('twlinks' ) ) ? '#'.$this->opt('twlinks' ) : '#0084b4';
 	$twitterbordercolor	= ( $this->opt('twbordercolor' ) ) ? '#'.$this->opt('twbordercolor') : '#444444';
+	if( $this->opt( 'twlanguage' )){$twitterlanguage = sprintf( 'lang="%s"', $this->opt( 'twlanguage' ));}else{$twitterlanguage ='';}
+	
 	
 	if ($twittermode == 'timeline' && $twitterautoexpandphotos == 'false' ) {$twitterwidgetid = '345561955005767680';}
 	if ($twittermode == 'timeline' && $twitterautoexpandphotos == 'true' ) {$twitterwidgetid = '345559937998524416';}
@@ -233,6 +274,7 @@ data-border-color="<?php echo $twitterbordercolor ?>"
 height="<?php echo $twitterheight ?>" 
 <?php echo $twitterdatatweetlimit ?>
 <?php echo $twitterdatachrome ?>
+<?php echo $twitterlanguage ?> 
 >Tweets by @<?php echo $twitterusername ?></a>
 <?php }
 	if ($twittermode == 'favorites'){?>
@@ -242,11 +284,12 @@ href="https://twitter.com/<?php echo $twitterusername ?>/favorites"
 data-widget-id="<?php echo $twitterwidgetid ?>" 
 data-favorites-screen-name="<?php echo $twitterusername ?>" 
 data-theme="<?php echo $twitterdarktheme ?>" 
-data-link-color="<?php echo $twitterlinks ?>" 
+data-link-color="<?php echo $twitterlinks ?>"
 data-border-color="<?php echo $twitterbordercolor ?>" 
 height="<?php echo $twitterheight ?>"
 <?php echo $twitterdatatweetlimit ?>
 <?php echo $twitterdatachrome ?>
+<?php echo $twitterlanguage ?> 
 >Favorite Tweets by @<?php echo $twitterusername ?></a>
 <?php }
 	if ($twittermode == 'list'){?>
@@ -262,6 +305,7 @@ data-border-color="<?php echo $twitterbordercolor ?>"
 height="<?php echo $twitterheight ?>" 
 <?php echo $twitterdatatweetlimit ?>
 <?php echo $twitterdatachrome ?>
+<?php echo $twitterlanguage ?> 
 >Tweets from @<?php echo $twitterusername ?>/<?php echo $twitterlist ?></a>
 <?php }
 	if ($twittermode == 'search'){?>
@@ -275,6 +319,7 @@ data-border-color="<?php echo $twitterbordercolor ?>"
 height="<?php echo $twitterheight ?>" 
 <?php echo $twitterdatatweetlimit ?>
 <?php echo $twitterdatachrome ?>
+<?php echo $twitterlanguage ?> 
 >Tweets about "<?php echo $twittersearch ?>"</a>
 <?php }?>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
--- twitter-all-in-one-dms.php
+++ twitter-all-in-one-dms.php
@@ -5,7 +5,7 @@ Author: MrFent
 Author URI: http://www.MrFent.com/twitter-all-in-one
 Demo: http://www.MrFent.com/twitter-all-in-one
 External: http://www.MrFent.com/twitter-all-in-one
-Version: 1.0.0
+Version: 1.0.1
 Description: Combines all four Twitter widgets: User timeline, Favorites, List, & Search
 Class Name: TwitterAllInOneDMS
 Workswith: templates, main, sidebar1, sidebar2, sidebar_wrap, header, footer, morefoot
